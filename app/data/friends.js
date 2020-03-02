03210fb1a068b561eaf8c872576d7d7070569efe
Lesly Sanchez <leslyf.sanchezm@gmail.com>
2020-03-01 23:06:49 -0600

    pasting files from original repo

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+  {
+    name: "Friend1",
+    photo: "https://via.placeholder.com/150",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Friend2",
+    photo: "https://via.placeholder.com/150",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  }
+];
+
+module.exports = friends
